729a81144ea49a17d778fbfb676ea4a2dcf751b4
Pramoth kumar <93421622+pramothkumarm@users.noreply.github.com>
2026-05-30 19:13:31 +0530

    Update logic.py

--- logic.py
+++ logic.py
@@ -16,7 +16,7 @@ from typing import List, Dict
 # CONFIG
 # ==========================================
 
-API_KEY = "AQ.Ab8RN6KavhRElFCSsg2JoPfyvAUtN3dVj7XxVm7xB9eHnWeBCw"
+API_KEY = "AQ.Ab8RN6L7fm9m21RxrBZFef_wRTe1qhIn84Sp6eM0VJfN3-gI5A"
 
 genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel(
